9787389f1450c6b35e87b017cacf381a6536130c
Yogendra Rana <yogendrarana4321@gmail.com>
2023-09-08 19:47:00 +0545

    Improved search bar

--- src/components/layout/Home.tsx
+++ src/components/layout/Home.tsx
@@ -4,8 +4,6 @@ function Home() {
     return (
         <section>
             <Navbar />
-
-            <p className="text-[5rem] mt-[50vh]">asdf</p>
         </section>
     )
 
--- src/components/utils/Search.tsx
+++ src/components/utils/Search.tsx
@@ -32,7 +32,7 @@ const Search = ({ isOpen, setOpen }: SearchProps) => {
                         type="text"
                         placeholder="I'm looking for..."
                         autoFocus
-                        className="py-[1rem] outline-none text-[2.25rem] placeholder:text-[2.25rem]"
+                        className="w-full pr-[5rem] py-[1rem] outline-none text-[2.25rem] placeholder:text-[2.25rem]"
                     />
                     <button className="text-[1.5rem]" type="button">
                         <i className="fa-solid fa-magnifying-glass"></i>
@@ -43,9 +43,9 @@ const Search = ({ isOpen, setOpen }: SearchProps) => {
             {/* overlay */}
             <div 
                 id="overlay" 
-                onClick={handleClick} 
+                onClick={handleClick}
                 className={`
-                    absolute top-0 left-0 right-0 bg-black/20 duration-150 h-[100vh]
+                    absolute top-0 left-0 right-0 bg-black/20 duration-300 h-[100vh]
                     ${isOpen ? "translate-y-0" : "translate-y-[-100%]"}`}>
             </div>
         </>
